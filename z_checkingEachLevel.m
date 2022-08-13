2d28b1463d0426b8788935117e1f7b1cb6e545ea
dizlex <37808439+dizlex@users.noreply.github.com>
2022-08-13 15:59:30 +0400

    Update z_checkingEachLevel.m

--- z_checkingEachLevel.m
+++ z_checkingEachLevel.m
@@ -1,14 +1,24 @@
+% this script allows the user to select a specific depth interval
+% plot it and save the result in a .txt file
+
 clear all;close all; clc;
 load('FinalCoh_def.mat')
-angle=0:180;
-V=1000;            % initial SCANNING VELOCITY in m/s
-LastVelocityToScan=5000; %the final Velocity to be scanned in m/s
-V_step=25;
-velocities=V:V_step:LastVelocityToScan; 
-[X,Y] =meshgrid(angle,velocities);
+clear all;close all; clc;
+load('seisXVfrom2600to3200Vstep20AngStep1WinSize=006Num_Rec=11DepthInterval=2940to3140FinalCoh.mat')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Select plotting bounds
+minVelocityToPlot=2600;
+maxVelocityToPlot=3100;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    [minDistance, indexOfMin] = min(abs(minVelocityToPlot-velocities));
+    [minDistance,indexOfMax]=min(abs(maxVelocityToPlot-velocities));
+    [X,Y] =meshgrid(angle,velocities(indexOfMin:indexOfMax));
     %contour(X,Y,FinalCoh(:,:,mastercount),'--')
-    Z=FinalCoh(:,:,25)';
-    Z=smoothdata(Z,'gaussian',6); %you need the curveFittingToolBox
+    Z=FinalCoh(:,indexOfMin:indexOfMax,1)'; %index of Min velocity, last dimension is slide number
+    %Z=circshift(Z,-10,2);
+    Z=smoothdata(Z,'gaussian',15); %you need the curveFittingToolBox
+    
     zmin = (min(Z(:))); 
     zmax = (max(Z(:)));
     zinc = (zmax - zmin) / 30; %choose the number of contours
@@ -21,14 +31,38 @@ velocities=V:V_step:LastVelocityToScan;
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %uncomment line below for surf plot
-    %surf(X,Y,FinalCoh(:,:,mastercount)')
+    %surf(X,Y,Z(:,:,1))
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     shading interp
-    colormap turbo
-    colorbar
-    title(sprintf('Vcoherency VS alpha top depth of:2391 and bottom of 2541'));
+    colormap turbo % selecting the color map
+    %colorbar
+    hCB=colorbar;
+    %hCB.Title.String='Title'
+    [n1,n2]=max(Z); %obtaining the [valueVector,PositionVector]
+    maxColorbar=max(n1); % obtaining the max value
+    set(hCB.XLabel,{'String','Rotation','Position'},{'Coherency',90,[-1 maxColorbar*0.50]}) %setting the colorbar label 
+    %title(sprintf('Vcoherency VS alpha top depth of:2300 and bottom of 2500'));
+    title('(b)','FontSize',14)
     xlabel('Angle (deg)')
     xticks(0:10:180)
     ylabel('Apparent Velocity (m/s)')
-    ylim([1750 2750])
+    ylim([minVelocityToPlot maxVelocityToPlot])
     view(2)
+    %%%%%%%%%%%%%%%%%%%%%%%%
+
+%% getting the slow event coordinates in angle and velocity
+    [maxFinCoh, AngIndexOfFinCoh] = max(FinalCoh);
+            %velIndex
+ [TrueMax, indTrueMax]=max(maxFinCoh); %obtaining the [valueVector,PositionVector]
+ maxCohVel_SlowEvent=velocities(indTrueMax) %the velocity coordinate of the max Coherency Value for the slow velocity
+ maxCohAng_SlowEvent=AngIndexOfFinCoh(indTrueMax) %the angle coordinate of the max Coherency value for the slow event
+ SlowEventCoh=FinalCoh(AngIndexOfFinCoh(indTrueMax),indTrueMax); %theValueAt (maxCohAng_slow,maxCohVel_slow)
+%% getting the fast event coordinates in angle and velocity
+ [maxFinCoh, AngIndexOfFinCoh] = max(FinalCoh(:,floor(length(velocities)/2):end)); %obtaining the [valueVector,PositionVector]
+            %velIndex
+ [TrueMax, indTrueMax]=max(maxFinCoh); %obtaining the [valueVector,PositionVector]
+
+ maxCohVel_FastEvent=velocities(floor(length(velocities)/2)+indTrueMax) %the velocity coordinate of the max Coherency Value for the slow velocity
+ maxCohAng_FastEvent=AngIndexOfFinCoh(indTrueMax) %the angle coordinate of the max Coherency value for the slow event
+ FastEventCoh=FinalCoh(AngIndexOfFinCoh(indTrueMax),indTrueMax); %theValueAt (maxCohAng_slow,maxCohVel_slow)
+ writematrix(FinalCoh,'FigureFileName.txt') %saving the result in a txt file
